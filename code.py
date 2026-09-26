1a0be37bea9f132fa7b483c28ba20a627cca7924
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-09-26 14:13:34 +0500

    add the part 4 of the python course

--- code.py
+++ code.py
@@ -44,8 +44,8 @@
 
 # String operations
 
-name = "Tony stark"
-grade = 'A'
+# name = "Tony stark"
+# grade = 'A'
 
 # print(name.upper()) # TONY STARK
 # print(name) # Tony stark # this is called Immutible, (means don't change orignal str)
@@ -99,13 +99,62 @@ grade = 'A'
 
 
 
-# Grading
+# # Grading
 
-marks = 60
+# marks = 60
 
-if marks >= 80:
-  print("Grade: A")
-elif marks < 80 and marks >= 60:
-  print("Grade: B")
-else:
-  print("Grade: C")
+# if marks >= 80:
+#   print("Grade: A")
+# elif marks < 80 and marks >= 60:
+#   print("Grade: B")
+# else:
+#   print("Grade: C")
+
+
+# # Range
+# num = range(0, 5)
+# print(num)
+
+# # range(start, stop, step)
+
+
+# # While Loop
+
+# i = 5
+# while i > 0:
+#   print(i * "*")
+#   i -= 1
+
+# print("End of Code")
+
+
+# For loop
+
+# num = range(5) # 0 to 5
+# for i in num:
+#   print(i)
+
+
+# for i in range(1, 6): # 1 to 5
+#   print(i)
+
+# even number
+
+# for i in range(1, 11):
+#   if i%2 == 0:
+#     print(i)
+
+
+# for i in range(2, 11, 2):
+#   print(i)
+
+# continue and breake key words
+
+# for i in range(1, 51):
+#   if(i == 21):
+#     continue # it skip the value of 21 and print the remaining 
+#     # break # it print the value upto 18 and will out from the loop and print the "End of loop".
+#   if(i % 3 == 0):
+#     print(i)
+
+# print("End of code")
---
+++ practice-ex4.py
@@ -0,0 +1,28 @@
+# print all odd number from the 1 to 20
+
+# for i in range(1, 20):
+#   if(i % 2 == 1):
+#     print(i)
+
+
+# # Print table of 57
+
+# for i in range(1, 11):
+#   print("57 X ", i , " = ", i * 57)
+
+
+# Print all multiples of 3
+# for i in range(3, 51, 3):
+#    if(i == 15) :
+#       continue
+#    print(i)
+
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+
+# for i in range(1, 1001):
+#   if(i % a == 0 and i % b == 0):
+#     print(i)
+#     break
+
+# print("Not found in the list")
